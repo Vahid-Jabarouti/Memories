9289219b49593ceaf9fe0f3117c5146d303f8b08
iamvahid <vahid.jabarouti@gmail.com>
2021-08-07 00:44:42 +0430

    providing Redux

--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ import PostRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', PostRoutes);
-
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+
+app.use('/posts', PostRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://vahid:vahid2357@cluster0.4pk3j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
@@ -18,6 +18,3 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true})
     .then(() => app.listen(PORT, () => console.log(`Server started on port ${PORT}`)))
     .catch(() => console.log("Connection failed!"));
-
-
-
