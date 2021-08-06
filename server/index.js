82ed6089e5fcdb1d15314b6038ba2edc9db5c630
iamvahid <vahid.jabarouti@gmail.com>
2021-08-06 19:14:06 +0430

    connecting to db

--- server/index.js
+++ server/index.js
@@ -9,3 +9,12 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
+const CONNECTION_URL = 'mongodb+srv://vahid:vahid2357@cluster0.4pk3j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true})
+    .then(() => app.listen(PORT, () => console.log(`Server started on port ${PORT}`)))
+    .catch(() => console.log("Connection failed!"));
+
+
+
