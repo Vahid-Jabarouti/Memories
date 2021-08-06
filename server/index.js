1101d130c51613abcd7e8765ac54e79a85314006
iamvahid <vahid.jabarouti@gmail.com>
2021-08-06 19:26:20 +0430

    adding routes

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ import express from 'express';
 import bodyParser from "body-parser";
 import mongoose from 'mongoose';
 import cors from 'cors';
+import PostRoutes from './routes/post.js';
 
 const app = express();
+
+app.use('/posts', PostRoutes);
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
